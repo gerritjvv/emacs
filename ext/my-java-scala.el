7ff01d4f16468a85796e1dd3d9172121534050b4
Gerrit Jansen van Vuuren <gerritjvv@gmail.com>
2017-05-13 20:21:12 +0200

    term tooling using fzf and perco, also add java mode linenum and flycheck

--- ext/my-java-scala.el
+++ ext/my-java-scala.el
@@ -66,8 +66,10 @@
 	  (lambda ()
 	    (editorconfig-mode t)      ;;reads the .editorconfig files and applies format, only to new files
 	    (meghanada-mode t)
+	    (line-number-mode t)
 	    (yas-minor-mode-on)
 	    (eldoc-mode t)
+	    (flycheck-mode t)
 	    (c-toggle-auto-newline 't) ;;automatically insert new lines where
 	    (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
@@ -76,23 +78,6 @@
 (add-hook 'groovy-mode-hook
 	  (lambda ()
 	    (gradle-mode t)))
-
-
-
-
-
-
-					;spaces_around_operators = true
-					;spaces_around_brackets = none
-
-					;curly_bracket_next_line = true
-					;indent_brace_style = Allman
-					;continuation_indent_size = 8
-
-;; (add-hook 'editorconfig-custom-hooks
-;; 	  '(lambda (props)
-;;props is a hashmap
-;; 	     (message (format " PROPS %s" props))))
 
 
 
--- install.sh
+++ install.sh
@@ -1,6 +1,31 @@
 #!/usr/bin/env bash
 
+##https://github.com/mooz/percol
+#brew install percol # python
+#brew install perco #percol but in go so faster
+## fzf is faster than percol
+https://github.com/junegunn/fzf
+brew install fzf
 
+/usr/local/opt/fzf/install
+
+source ~/.bashrc  # bash
+source ~/.zshrc   # zsh
+
+#wget https://github.com/changyuheng/zsh-interactive-cd/blob/master/zsh-interactive-cd.plugin.zsh
+curl https://raw.githubusercontent.com/changyuheng/zsh-interactive-cd/master/zsh-interactive-cd.plugin.zsh > ~/zsh-interactive-cd.plugin.zsh
+echo "source ~/zsh-interactive-cd.plugin.zsh" >> ~/.zshrc
+echo "source ~/zsh-interactive-cd.plugin.zsh" >> ~/.profile
+
+source ~/.zshrc
+
+### setup bashrc for macosx
+
+if ! grep -q "source ~/.bashrc" ~/.bash_profile;
+then
+ echo "source ~/.bashrc" >> ~/.bash_profile
+fi
+ 
 brew install cask
 
 brew cask install emacs
@@ -25,3 +50,4 @@ brew install rg
 brew install sbt
 
 brew install editorconfig
+
